079599fe302579f3807f387a3158afe05affa170
alicagan388-png <alicagan388@gmail.com>
2026-04-05 16:57:41 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,16 @@
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.AIzaSyDjLfnp-i3SKJoDUd0Axj2JwM3KLPUObfA': JSON.stringify(env.AIzaSyDjLfnp-i3SKJoDUd0Axj2JwM3KLPUObfA),
-        'process.env.AIzaSyDjLfnp-i3SKJoDUd0Axj2JwM3KLPUObfA': JSON.stringify(env.AIzaSyDjLfnp-i3SKJoDUd0Axj2JwM3KLPUObfA)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+export default defineConfig({
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
+    },
+  },
 });
